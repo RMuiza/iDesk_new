3cc258dbe4f06f5294d2f45b020c273db67dd7c8
Muiza <raihana@vommex.com>
2020-07-07 14:31:16 +0530

    init

--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -15,7 +15,7 @@ export class Login extends React.Component{
                 </div>
                 <div className="form">
                     <div className="form-group">
-                        <label htmlFor="username">User Name  adnhggfft</label>
+                        <label htmlFor="username">User Name  adnh</label>
                          <input type="text" name="username" placeholder="username"/>
                     </div>
                     <div className="form-group">
